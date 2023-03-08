8c6fc44d6d885d918b8fe61c6d322496f55478aa
77oda <m.abdelhamed941@gmail.com>
2023-03-08 12:19:16 +0200

    add job form by user

--- job/models.py
+++ job/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 '''
@@ -18,6 +19,7 @@ JOB_TYPE =(
     ('Part Time','Part Time'),
 )
 class Job (models.Model): # table
+    owner = models.ForeignKey(User, related_name='job_owner', on_delete=models.CASCADE)
     title = models.CharField(max_length=100) # colum
     # Location
     job_type = models.CharField(max_length=15,choices=JOB_TYPE)
@@ -43,3 +45,14 @@ class Category (models.Model):
         return self.name
     
     
+class Apply (models.Model):
+    job = models.ForeignKey(Job, related_name='apply_job', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    website = models.URLField()
+    cv = models.FileField(upload_to='apply/')
+    cover_letter= models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
--- job/views.py
+++ job/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from.models import Job
 from django.core.paginator import Paginator
+from.form import ApplyForm ,JobForm
 
 
 # Create your views here.
 
 def job_list(request):
     job_list = Job.objects.all()
-    paginator = Paginator(job_list, 1) # Show 25 contacts per page.
+    paginator = Paginator(job_list, 3) # Show 25 contacts per page.
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     
@@ -16,5 +18,28 @@ def job_list(request):
 
 def job_detail(request,slug):
     job_detail=Job.objects.get(slug=slug)
-    context={'job':job_detail} 
+    
+    if request.method=='POST':
+        form = ApplyForm(request.POST , request.FILES)
+        if form.is_valid():
+            myform= form.save(commit=False)
+            myform.job=job_detail
+            myform.save()
+    else:
+        form =ApplyForm()
+    
+    context={'job':job_detail , 'form':form} 
     return render(request,'job/job_details.html',context)
+
+def add_job (request):
+    if request.method=='POST':
+        form = JobForm(request.POST , request.FILES)
+        if form.is_valid():
+            myform= form.save(commit=False)
+            myform.owner=request.user
+            myform.save()
+            return redirect(reverse('jobs:job_list'))
+    else:
+        form =JobForm()
+    
+    return render(request, 'job/add_job.html',{'form':form})
